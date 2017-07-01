41a51dd744328338a4299cddfc7ba41042495933
Yang <sheep-sheep@users.noreply.github.com>
2017-06-30 23:22:17 -0400

    Create Asignment 4.py

---
+++ Applied Machine Learning in Python/Asignment 4.py
@@ -0,0 +1,22 @@
+Reusability matters
+
+1. Separate the data into distinct groups by similarity
+2. Trees often require less preprocessing of data
+   Trees are easy to interpret and visualize
+3. To improve generalization by reducing correlation among the trees and making the model more robust to bias.
+4. Neural Networks wrong
+   Naive Bayes wrong
+5. For a model that won’t overfit a training set, Naive Bayes would be a better choice than a decision tree.
+   For predicting future sales of a clothing line, Linear regression would be a better choice than a decision tree regressor
+6. Neural Network
+   KNN (k=1)
+   Decision Tree
+7. 
+8. compliance_detail - More information on why each ticket was marked compliant or non-compliant
+   collection_status - Flag for payments in collections
+9. Remove variables that a model in production wouldn’t have access to wrong
+   Sanity check the model with an unseen validation set wrong
+   
+If time is a factor, remove any data related to the event of interest that doesn’t take place prior to the event.
+Ensure that data is preprocessed outside of any cross validation folds.
+10. 0110
